37b1ac7bd0c1173d3e264b1047ee8dfc19acf53c
João Gilberto <gjoao11.dev@gmail.com>
2025-11-19 21:35:32 -0300

    Organizar imports em Merchant

--- src/main/java/edu/ucsal/fiadopay/domain/Merchant.java
+++ src/main/java/edu/ucsal/fiadopay/domain/Merchant.java
@@ -1,7 +1,10 @@
 package edu.ucsal.fiadopay.domain;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
